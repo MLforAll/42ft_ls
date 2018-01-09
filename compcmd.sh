c06983c7a891525501afcb5071b26afbe945df45
Kelian DUMARAIS <kdumarai@e1r5p7.42.fr>
2018-01-09 17:12:17 +0100

    Updated compcmd.sh

--- compcmd.sh
+++ compcmd.sh
@@ -3,4 +3,4 @@ gflags="-Wall -Werror -Wextra"
 if [ "$1" == "nf" ]; then
 	gflags=""
 fi
-gcc $gflags -o ft_ls srcs/*.c /Users/kdumarai/Documents/42_main_cursus/Libft/libft.a -I includes -I /Users/kdumarai/Documents/42_main_cursus/Libft
+gcc $gflags -o ft_ls `find srcs -type f -name "*.c"` /Users/kdumarai/Documents/42_main_cursus/Libft/libft.a -I includes -I /Users/kdumarai/Documents/42_main_cursus/Libft
