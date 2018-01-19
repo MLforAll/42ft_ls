6e190b7ba96c9d7e4bb950b2ad4e40705a0c4291
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-01-19 06:07:31 +0100

    Fixed no such file / not a dir err

--- srcs/ls_misc.c
+++ srcs/ls_misc.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 01:51:57 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/01/19 05:27:55 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/01/19 06:06:14 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ void		ls_elem_err(char *path, int err)
 {
 	char	*elem_name;
 
-	elem_name = ft_strrchr(path, '/');
-	elem_name = (!elem_name) ? path : elem_name + 1;
+	if (err == ENOENT || err == ENOTDIR)
+		elem_name = path;
+	else
+	{
+		elem_name = ft_strrchr(path, '/');
+		elem_name = (!elem_name) ? path : elem_name + 1;
+	}
 	ft_lsprint_fd(2, "%s: %s: %s\n", PRGM_NAME, elem_name, strerror(err));
 }
 
